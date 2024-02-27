48c15cb85fc47ffc185c9465214311959f9480c1
brainbytescreative1 <taylor.martin+github@brainbytescreative.com>
2024-02-27 14:15:47 -0500

    add class to custom html element

--- template-parts/elements/html.php
+++ template-parts/elements/html.php
@@ -9,11 +9,17 @@ if( get_row_layout() == 'custom_html' ):
     $html_classes[] = 'element';
     $html_classes[] = 'custom-html';
 
+    // classes
+    $additional_classes = get_sub_field('additional_classes');
+    if ( $additional_classes ) {
+        $html_classes[] = $additional_classes;
+    }
+
     // spacing
     $html_classes[] = get_spacing_bbc(get_sub_field('html_spacing'));
 
     // convert array to string
-    $html_classes = implode(' ', $html_classes);
+    $html_classes = trim(implode(' ', $html_classes));
 
     echo '<div class="'. esc_attr($html_classes) .'">'; // html element start
 
