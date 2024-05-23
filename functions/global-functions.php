859bc205bde69260d16573b50db773feef435596
brainbytescreative1 <taylor.martin+github@brainbytescreative.com>
2024-05-23 15:03:45 -0400

    fix get advanced

--- functions/global-functions.php
+++ functions/global-functions.php
@@ -146,6 +146,9 @@ function get_background_bbc($field, $classes, $styles, $sub = false) {
 
                         if ( $background_image_format === 'optimized' ) {
 
+                            /* webp test */
+                            $handle = curl_init($image_mobile . '.webp');
+                            
                             $image_mobile = isUrlValid($image_mobile);
 
                         }
@@ -1095,6 +1098,57 @@ function get_menu_padding_bbc($field, $classes, $styles) {
     } else {
         return null;
     }
+}
+
+function get_advanced_bbc($field = false, $sub = true, $classes = false, $id = false) {
+
+    if ( $field ) {
+        if ( $sub = false ) {
+            $field = get_field($field);
+        } else {
+            $field = get_sub_field($field);
+        }
+    }   
+
+    // initialize
+    $advanced_classes = [];
+    $advanced_id = '';
+
+    if ( $field ) {
+        // classes
+        if ( $field['classes'] ) {
+            $advanced_classes[] = $field['classes'];
+        }
+        if ( $field['hide_desktop'] ) {
+            $advanced_classes[] = 'desktop-hide';
+        }
+        if ( $field['hide_tablet'] ) {
+            $advanced_classes[] = 'tablet-hide';
+        }
+        if ( $field['hide_mobile'] ) {
+            $advanced_classes[] = 'mobile-hide';
+        }
+        // id
+        if ( $field['unique_id'] ) {
+            $advanced_id = trim( $field['unique_id'] );
+            $advanced_id = $str=preg_replace('/\s+/', '', $advanced_id);
+        }
+    }
+
+    // process
+    $advanced = array(
+        'classes' => null,
+        'id' => null,
+    );
+    if ( $advanced_classes ) {
+        $advanced['classes'] = trim(implode(' ', $advanced_classes));
+    }
+    if ( $advanced_id ) {
+        $advanced['id'] = $advanced_id;
+    }
+
+    return $advanced;
+
 }
 
 require_once( __DIR__ . '/global-functions/image-optimize.php');
